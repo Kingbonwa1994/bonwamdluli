11af39fa8360aae2e2b759d28e7808fba184fb6d
Kingbonwa1994 <bonwamdluli@gmail.com>
2024-06-14 18:06:11 +0200

    hero redesigned

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { Hero } from "@/components/hero";
 
 export default function Home() {
   return (
-    <main className="flex h-screen flex-col items-center justify-between p-24">
+    <main className="flex h-screen flex-col items-center justify-between">
       <Hero />
     </main>
   );
--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,14 +1,23 @@
 import React from "react";
 import { BackgroundGradientAnimation } from "@/components/ui/background-gradient-animation";
+import Image from "next/image";
 
 export function Hero() {
+  const svgContent = '/spin.svg';
+  const heroImage = '/logs.png';
   return (
-    <BackgroundGradientAnimation>
-      <div className="absolute z-50 inset-0 flex items-center justify-center text-white font-bold px-4 pointer-events-none text-3xl text-center md:text-4xl lg:text-7xl">
-        <p className="bg-clip-text text-transparent drop-shadow-2xl bg-gradient-to-b from-white/80 to-white/20">
-          Bonwa Mdluli - UX Architect /// spinner and the avatar 
-        </p>
+    <BackgroundGradientAnimation className="my-0 py-0 flex items-center justify-center h-screen">
+       <div className="relative z-10">
+       <Image
+        src={heroImage}
+        alt="Hero image"
+        width={300}
+        height={300}
+        objectFit="fit"
+        className="object-center" // Center the image
+      />
       </div>
+      <Image src={svgContent} alt="hero" layout="fill" objectFit="cover" />
     </BackgroundGradientAnimation>
   );
 }
