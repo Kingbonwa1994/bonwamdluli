2e62933fe13eb7703dd855436c654758e3850cce
Kingbonwa1994 <bonwamdluli@gmail.com>
2024-11-22 02:41:15 +0200

    tech-stack icons fix

--- src/components/tech-stack.tsx
+++ src/components/tech-stack.tsx
@@ -2,23 +2,30 @@
 
 import { motion } from "framer-motion"
 import { Icon } from "@/components/icon"
+import { GitCompareArrows } from 'lucide-react'
+import { FaReact } from "react-icons/fa";
+import { RiNextjsFill, RiSupabaseLine, RiDrizzleLine } from "react-icons/ri";
+import { SiVercel } from "react-icons/si";
+import { DiMongodb } from "react-icons/di";
+import { FaDocker } from "react-icons/fa6";
+import { SiPayloadcms } from "react-icons/si";
 
 const technologies = [
-  { name: "React", icon: "react" },
-  { name: "Next.js", icon: "nextjs" },
-  { name: "Git", icon: "git" },
-  { name: "Vercel", icon: "vercel" },
-  { name: "Supabase", icon: "supabase" },
-  { name: "Payload", icon: "payload" },
-  { name: "Drizzle", icon: "drizzle" },
-  { name: "MongoDB", icon: "mongodb" },
-  { name: "Docker", icon: "docker" },
+  { name: "React", icon: <FaReact /> },
+  { name: "Next.js", icon: <RiNextjsFill /> },
+  { name: "Git", icon: GitCompareArrows },
+  { name: "Vercel", icon: <SiVercel /> },
+  { name: "Supabase", icon: <RiSupabaseLine /> },
+  { name: "Payload", icon: <SiPayloadcms /> },
+  { name: "Drizzle", icon: <RiDrizzleLine /> },
+  { name: "MongoDB", icon: <DiMongodb /> },
+  { name: "Docker", icon: <FaDocker /> },
 ]
 
 export function TechStack() {
   return (
     <section className="h-screen w-full flex items-center justify-center relative overflow-hidden snap-start">
-      <div className="absolute inset-0 bg-gradient-to-br from-purple-900 to-pink-400 animate-gradient-x"></div>
+      <div className="absolute inset-0 bg-gradient-to-br from-purple-900 via-green-950 to-pink-400 animate-gradient-x"></div>
       <div className="z-10 grid grid-cols-3 gap-8 p-8">
         {technologies.map((tech, index) => (
           <motion.div
